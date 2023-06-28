ac9bd63be35da759727ee26d9da96a41226c69a2
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-06-28 18:34:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,3 +27,20 @@ const handleGameOver = () => {
     alert("Game Over! Press OK to replay...");
     location.reload();
 }
+
+const changeDirection = e => {
+    // Changing velocity value based on key press
+    if(e.key === "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if(e.key === "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if(e.key === "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if(e.key === "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
